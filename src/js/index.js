9a94266988460411f4a8dec6d3c50c8371ca8460
Roberto Wagner <112600847+RobertoWag@users.noreply.github.com>
2022-10-25 14:33:00 -0300

    commit inicial
    Meu primeiro portfólio inicial...

---
+++ src/js/index.js
@@ -0,0 +1,42 @@
+/*
+
+
+
+*/
+
+// passo 1 - 
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach(aba => {
+    // passo 2 -
+    aba.addEventListener("click", function () {
+
+        if (aba.classList.contains("selecionado")) {
+            return;
+        }
+
+        selecionarAba(aba)
+        mostrarInformacoesDaAba(aba)
+    });
+});
+
+function selecionarAba(aba) {
+    // passo 3 -
+    const abaSelecionada = document.querySelector(".aba.selecionado");
+    abaSelecionada.classList.remove("selecionado")
+
+    // passo 4 -
+    aba.classList.add("selecionado")
+}
+
+function mostrarInformacoesDaAba(aba) {
+    // passo 5 -
+    const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+    informacaoSelecionada.classList.remove("selecionado");
+
+    // passo 6 - 
+    const idDoElementoDeinformacoesDaAba = `informacao-${aba.id}`
+
+    const informacaoASerMostrada = document.getElementById(idDoElementoDeinformacoesDaAba)
+    informacaoASerMostrada.classList.add("selecionado")
+}
